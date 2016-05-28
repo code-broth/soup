45b7f2ced1b40085cb0dcbd16e60067e798fefb2
Andrew Dirksen <bddap@Andrews-MacBook-Air.local>
2016-05-28 12:22:23 -0700

    three.js to dependencies, cube_example to separate file

--- Client/client.js
+++ Client/client.js
@@ -1,11 +1,12 @@
-const electron = require('electron');
-const {app} = electron;
-const {BrowserWindow} = electron;
+const electron = require('electron')
+const {app} = electron
+const {BrowserWindow} = electron
 
 function createWindow() {
   win = new BrowserWindow({fullscreen:true});
 
-  win.loadURL(`file://${__dirname}/index.html`);
+  win.loadURL(`file://${__dirname}/index.html`)
+
   /*win.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
